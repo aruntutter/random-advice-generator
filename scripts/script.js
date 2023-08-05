5833b36e4f2c1fa260d155ef1d69a738ace4cde1
Arun Kumar R <aruntutter19@gmail.com>
2023-08-05 14:23:13 +0530

    js structure added

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
-// global variables
+// get elements
+const quoteIdEl = document.getElementById("quote-id");
+const quoteTextEl = document.getElementById("quote-text");
+const btnRandom = document.getElementById("btn-random");
 
 // functions
 const init = () => {};
 
 //event listeners
+btnRandom.addEventListener("click", () => {});
 
 // initial settings
 init();
