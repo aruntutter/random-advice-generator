df2d264b0666dc809dbfdb507cb3fc59f4ed0b74
Arun Kumar R <aruntutter19@gmail.com>
2023-08-05 15:32:24 +0530

    final code

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,31 @@ const quoteIdEl = document.getElementById("quote-id");
 const quoteTextEl = document.getElementById("quote-text");
 const btnRandom = document.getElementById("btn-random");
 
+// global variables
+const url = `https://api.adviceslip.com/advice`;
+
 // functions
-const init = () => {};
+function init() {
+  getRandomAdvice();
+}
+
+async function getRandomAdvice() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    quoteIdEl.innerText = `#${data.slip.id}`;
+    quoteTextEl.innerText = `"${data.slip.advice}"`;
+
+    // console.log(data.slip.id);
+    // console.log(data.slip.advice);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
 
 //event listeners
-btnRandom.addEventListener("click", () => {});
+btnRandom.addEventListener("click", getRandomAdvice);
 
 // initial settings
 init();
